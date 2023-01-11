dbe93904256e216220cab7d95fbf347a19534559
Firaol <maxalex108@gmail.com>
2023-01-10 19:41:46 -0800

    Basic layout arranged

---
+++ src/skeleton/Skeleton.java
@@ -0,0 +1,35 @@
+
+package skeleton;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+
+public class Skeleton extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        javafx.geometry.Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+        System.out.println(screenBounds.getHeight()  + " " + screenBounds.getWidth());
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.setWidth(screenBounds.getWidth());
+        stage.setHeight(screenBounds.getHeight());
+        
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
