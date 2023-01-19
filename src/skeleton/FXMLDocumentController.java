c48475f54b2357cbb5875320ce989eb73cc15136
Firaol <maxalex108@gmail.com>
2023-01-18 18:45:27 -0800

    Added a cut option , not implemented yet

--- src/skeleton/FXMLDocumentController.java
+++ src/skeleton/FXMLDocumentController.java
@@ -6,8 +6,11 @@
 package skeleton;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 
@@ -17,8 +20,12 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -35,12 +42,20 @@ public class FXMLDocumentController implements Initializable {
     private  MediaPlayer mediaplayer;
     boolean playing = false;
     private InvalidationListener listener;
+    public double[] cutTime = new double[2];
+   
     private String filepath;
     // TimeLine Slider
     @FXML
     private Slider tSlider;
     @FXML
+    private ImageView timeLineImage;
+    @FXML
     private Slider vSlider;
+    @FXML
+    private Slider tSliderEnd;
+    @FXML
+    private Button trim;
     @FXML
     private Label label;
 //    @FXML
@@ -48,7 +63,7 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private MediaView mediaview;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
+    private void handleButtonAction(ActionEvent event) throws IOException {
         
         FileChooser chooser = new FileChooser();
         //This is used to filter the extentions
@@ -64,6 +79,23 @@ public class FXMLDocumentController implements Initializable {
             mediaplayer = new MediaPlayer(media);
             // Add the mediaplayer to media view
             mediaview.setMediaPlayer(mediaplayer);
+            
+     
+            try {
+                if(new File("001.jpg").exists()){
+                    
+                }else{
+               // Process split = Runtime.getRuntime().exec("cmd /c start " +"ffmpeg -i "+ file + " -r 1/20 %03d.jpg");
+                Process stitch = Runtime.getRuntime().exec("cmd /k start " +"ffmpeg -i %03d.jpg  -filter_complex \"scale=80:-1,tile=20x1:padding=6\" output2.png");
+//                
+                }
+                
+               // timeLineImage.setImage(new Image("C:\\Users\\SWL\\Documents\\NetBeansProjects\\Skeleton\\output.png"));
+            } catch (IOException ex) {
+                Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+           
             
            // This is used to synchronize the video player with the tSlider
             mediaplayer.currentTimeProperty().addListener(new ChangeListener<Duration>(){
@@ -137,14 +169,29 @@ public class FXMLDocumentController implements Initializable {
        System.exit(0);
     }
   
+    @FXML
+    private void handleCut(ActionEvent event){
+        tSliderEnd.setVisible(true);
+        trim.setVisible(true);
+        System.out.println(tSlider.getValue());
+    }
     
+     @FXML
+    private void handleTrim(ActionEvent event){
+       double trimStart = tSlider.getValue();
+       double trimEnd = tSliderEnd.getValue();
+       
+       System.out.println("The cut is going to be from :" +trimStart + " - " + trimEnd );
+       
+        tSliderEnd.setVisible(false);
+        trim.setVisible(false);
+    }
    
    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        tSlider.showTickMarksProperty().setValue(true);
-        tSlider.showTickLabelsProperty().setValue(true);
-       
+        tSliderEnd.setVisible(false);
+        trim.setVisible(false);
         
     }     
     
